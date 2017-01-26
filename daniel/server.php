c602c6309d34a5c53aa3067538aa29d1e48ee2e6
DanielSrb <srb.dan@gmail.com>
2017-01-26 14:00:50 +0100

    daniels folder is back again

---
+++ daniel/server.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Daniel's home</title>
+    <link rel="stylesheet" href="dist/css/daniel.css">
+
+</head>
+<body>
+
+<h1>I am using this PHP configuration</h1>
+
+<?php phpinfo(); ?>
+
+</body>
+</html>
